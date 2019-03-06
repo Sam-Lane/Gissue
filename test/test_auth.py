f3f4d0fceccd2147aaf97ff6bc02bb5f984c9358
Sam Lane <samuel.lane@hotmail.com>
2019-03-06 10:57:58 +0000

    added tests to check auth

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,4 +1,30 @@
+import os
+import requests
+
 import pytest
 
-def test_test():
-    assert 2, 1+1
+
+from gissue.auth import Auth
+
+@pytest.mark.order1
+def test_update_token():
+    if 'TRAVIS' in os.environ:
+        auth = Auth()
+        auth.update_token(os.environ['TOKEN'])
+
+        assert os.path.exists('.gissue')
+    else:
+        pass
+
+@pytest.mark.order2
+def test_authed():
+    auth = Auth()
+    params = {'access_token' : auth.get_token()}
+
+    home = os.path.expanduser("~")
+    gissueFile = os.path.join(home, ".gissue")
+    assert os.path.isfile(gissueFile)
+    
+    r = requests.get('https://api.github.com/user', params=params)
+    
+    assert r.status_code == 200
